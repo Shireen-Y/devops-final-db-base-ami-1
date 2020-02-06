4168661289c23c05eb1adee1f161f60ab2839650
Shireen Yawer <SYawer@spartaglobal.com>
2020-02-06 11:58:00 +0000

    license added

--- berks-cookbooks/client.rb
+++ berks-cookbooks/client.rb
@@ -1,2 +1,9 @@
 chef_license "{{.accept}}"
 # chef_server_url "{{.https://localhost:80}}"
+
+
+# {
+#   "type": "chef-client",
+#   "config_template": "./berks-cookbooks/client.rb",
+#   "server_url": "https://localhost:80"
+# }
